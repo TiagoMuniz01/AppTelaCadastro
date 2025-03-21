a707ad882fe663d785c5c572f16b3d6c8abed1b1
Tiago Pimentel Muniz <131329736+TiagoMuniz01@users.noreply.github.com>
2025-03-21 12:19:47 -0300

    VersãoFinal

--- app/src/main/java/com/example/apptelacadastro/MainActivity.kt
+++ app/src/main/java/com/example/apptelacadastro/MainActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,19 +31,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.apptelacadastro.ui.theme.AppTelaCadastroTheme
-import com.example.apptelacadastro.ui.theme.primaryBlue
-import com.example.apptelacadastro.ui.theme.terciaryBlue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,21 +45,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTelaCadastroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen()
+                    TelaInicial()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun HomeScreen() {
-    var name by remember { mutableStateOf("") }
+fun TelaInicial() {
+    var nome by remember { mutableStateOf("") }
     var telefone by remember { mutableStateOf("") }
     var curso by remember { mutableStateOf("") }
     var serie by remember { mutableStateOf("") }
-
 
     Column(
         Modifier.fillMaxSize(1f),
@@ -80,31 +70,31 @@ fun HomeScreen() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
-                value = name,
-                onValueChange = {name = it},
+                value = nome,
+                onValueChange = { nome = it },
                 label = { Text("Nome Completo") }
             )
             Spacer(Modifier.height(16.dp))
             OutlinedTextField(
                 value = telefone,
-                onValueChange = {telefone = it},
+                onValueChange = { telefone = it },
                 label = { Text("Telefone") }
             )
             Spacer(Modifier.height(16.dp))
             OutlinedTextField(
                 value = curso,
-                onValueChange = {curso = it},
+                onValueChange = { curso = it },
                 label = { Text("Curso") }
             )
             Spacer(Modifier.height(16.dp))
             OutlinedTextField(
                 value = serie,
-                onValueChange = {serie = it},
+                onValueChange = { serie = it },
                 label = { Text("Série") }
             )
             Spacer(Modifier.height(24.dp))
             Button(
-                onClick = { }
+                onClick = { /* Ação de envio */ }
             ) {
                 Text(text = "Enviar Dados")
             }
@@ -120,21 +110,10 @@ fun Avatar() {
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            Modifier
-                .height(200.dp)
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            terciaryBlue, primaryBlue
-                        )
-                    )
-                )
-                .fillMaxWidth()
-        ) {
+        Box() {
             Image(
                 painter = painterResource(R.drawable.avatar_image),
-                contentDescription = "avatar image",
+                contentDescription = "imagem de avatar",
                 Modifier
                     .offset(y = 50.dp)
                     .clip(shape = CircleShape)
@@ -149,59 +128,5 @@ fun Avatar() {
             fontSize = 32.sp,
             fontWeight = FontWeight.SemiBold
         )
-    }
-}
-
-
-
-// function for another view (pobably a view about user informations)
-@Composable
-fun AvatarImage() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(200.dp)
-    ) {
-        Box(
-            Modifier
-                .height(100.dp)
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            terciaryBlue, primaryBlue
-                        )
-                    )
-                )
-                .fillMaxWidth()
-        ) {
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = "avatar image",
-                Modifier
-                    .offset(y = 50.dp)
-                    .clip(shape = CircleShape)
-                    .align(Alignment.BottomCenter)
-            )
-        }
-        Spacer(Modifier.height(50.dp))
-        Column(
-            Modifier
-                .padding(16.dp)
-        ) {
-            Text(
-                text = "Person Name",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = "Description of person",
-                Modifier
-                    .padding(top = 8.dp),
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Normal
-            )
-        }
     }
 }
--- app/src/main/java/com/example/apptelacadastro/ui/theme/Color.kt
+++ app/src/main/java/com/example/apptelacadastro/ui/theme/Color.kt
@@ -10,6 +10,3 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val primaryBlue = Color(0xff1195ff)
-val secondaryBlue = Color(0xff5db7ff)
-val terciaryBlue = Color(0xff084b80)
